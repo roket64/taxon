397da25a08c7b66b34c85dd7172b5c780218376f
roket64 <1p41i4y1@gmail.com>
2025-02-28 17:48:08 +0900

    dev

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
-use crate::api::ApiHandler;
-use api::schema::WorldStateKind;
+use api::schema::*;
 use log::debug;
+use strum::IntoEnumIterator;
+use taxonlib::api::{self, ApiHandler};
 
 #[tokio::main]
 async fn main() {
     // load enviroment vars
     dotenv::dotenv().ok();
 
-    // initiate logging crate using file
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
 
-    let kind = WorldStateKind::ArchonHunt;
-    let handler = ApiHandler::new();
-    let what = handler.fetch(&kind).await;
-    debug!("what: {:#?}", what);
+    for kind in WorldStateKind::iter() {
+        debug!("fetching {:#?}...", &kind);
+        let worldstate = ApiHandler::fetch_worldstate_contents(&kind, Some("langugae=kr")).await;
+        debug!("parsed: {:#?}", worldstate);
+    }
 }
-
-pub mod api;
-pub mod utils;
